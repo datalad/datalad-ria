8bd86ab0a5b1fe5ea7f6b5ee368bdf08f5366d13
Michael Hanke <michael.hanke@gmail.com>
2023-09-11 16:34:11 +0200

    Basic `ria_sshserver` fixture
    An included test documents its use -- basically an info-dict has all the
    properties necessary to gain write access.
    
    The default configuration is SSH-login to localhost over a standard
    system SSHD. All SSH testing via this fixture is skipped when
    DATALAD_TESTS_SSH is not set.

---
+++ datalad_ria/tests/test_fixtures.py
@@ -0,0 +1,14 @@
+from .fixtures import verify_ssh_access
+
+
+def test_riaserver_fixture(ria_sshserver):
+    # we run the same test that the fixture already ran, to verify that
+    # the necessary information comes out of the fixture in a usable manner
+    verify_ssh_access(
+        ria_sshserver['HOST'],
+        ria_sshserver['SSH_PORT'],
+        ria_sshserver['SSH_LOGIN'],
+        ria_sshserver['SSH_SECKEY'],
+        ria_sshserver['SSH_PATH'],
+        ria_sshserver['LOCALPATH'],
+    )
