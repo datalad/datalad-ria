01953276dcee36082640df1c44295feaa026db22
Michael Hanke <michael.hanke@gmail.com>
2023-09-28 07:05:37 +0200

    Decomplexify dependencies of initremote test
    It does not actually need a store to be present.

--- datalad_ria/tests/test_ora.py
+++ datalad_ria/tests/test_ora.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytest
 import re
 
@@ -9,10 +10,10 @@ from datalad_next.runners import (
 ora_external_type = 'ora2'
 
 
-def test_ora_initremote_errors(ria_store_localaccess, existing_dataset):
+def test_ora_initremote_errors(existing_dataset):
     ds = existing_dataset
     repo = ds.repo
-    _, store_path = ria_store_localaccess
+    store_path = Path.cwd()
 
     ir_cmd = [
         'initremote',
@@ -33,6 +34,20 @@ def test_ora_initremote_errors(ria_store_localaccess, existing_dataset):
     ):
         # use a file:// url here
         repo.call_annex(ir_cmd + [f'url={store_path.as_uri()}'])
+
+
+def test_ora_localops(ria_store_localaccess, populated_dataset):
+    ds = populated_dataset
+    repo = ds.repo
+    _, store_path = ria_store_localaccess
+
+    ir_cmd = [
+        'initremote',
+        f'test-{ora_external_type}',
+        'type=external',
+        f'externaltype={ora_external_type}',
+        'encryption=none',
+    ]
 
     # smoke test that it can run
     repo.call_annex(ir_cmd + [f'url=ria+{store_path.as_uri()}'])
